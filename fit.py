1d6bf60ed2dfaec1bc2c4eec8d4dbfed2baaf824
Bartosz Balcerzak <bartosz.maciej.balcerzak@gmail.com>
2018-07-09 00:38:41 +0200

    fit: add input argumet parsing

--- fit.py
+++ fit.py
@@ -6,19 +6,37 @@ u_ref = 3.3
 rx = 8.03
 
 def adc2u(adc_val, u_ref, adc_res):
-    return (u_ref / ((2**adc_res) - 1)) * adc_val
+	return (u_ref / ((2**adc_res) - 1)) * adc_val
 
 def adc2r(adc_val, u_ref, adc_res, rx):
 	return ((u_ref * rx) / adc2u(adc_val, u_ref, adc_res)) - rx
 
 def main():
 
-    f = open('adc2u.dat' ,'w+')
-    f.write("#adc_val      u       r\n")
-    for x in range(1, 2**adc_res):
-        line = '%-10d %4f %12f\n' % (x, adc2u(x, u_ref, adc_res), adc2r(x, u_ref, adc_res, rx))
-        f.write(line)
-    f.close()
+	parser = ap.ArgumentParser()
+	parser.add_argument("-a",
+		"--adc_res",
+		type = int,
+		help = "ADC resolution (default 10 bits)",
+		default = 10)
+	parser.add_argument("-r",
+		"--u_ref",
+		type = float,
+		help = "ADC reference voltage (default 3.3V)",
+		default = 3.3)
+	parser.add_argument("-rx",
+		"--resistance",
+		type = float,
+		help = "resistance of resistor in series with termistor in Ohms (default 10k)",
+		default = 10000)
+	args = parser.parse_args()
+
+	f = open('adc2u.dat' ,'w+')
+	f.write("#adc_val      u       r\n")
+	for x in range(1, 2**args.adc_res):
+		line = '%-10d %4f %12f\n' % (x, adc2u(x, args.u_ref, args.adc_res), adc2r(x, args.u_ref, args.adc_res, args.resistance))
+		f.write(line)
+	f.close()
 
 if __name__ == "__main__":
-    main()
+	main()
